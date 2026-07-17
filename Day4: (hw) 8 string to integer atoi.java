b281a107e4fa5350bb9d40289a1856c0d7d77f53
rohinimurugan02 <rohinimurugan02@gmail.com>
2026-07-17 13:12:39 +0530

    Implement string to integer conversion (atoi)

---
+++ Day4: (hw) 8 string to integer atoi.java
@@ -0,0 +1,38 @@
+public class stringtointegeratoi  {
+    public int myAtoi(String s) {
+        if (s == null) return 0;
+
+        int n = s.length();
+        int i = 0;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i == n) return 0;
+
+        int sign = 1;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        int result = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (result > Integer.MAX_VALUE / 10 ||
+                (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+}
