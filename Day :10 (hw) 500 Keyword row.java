8bc2e4fd85533e93f908549580de95f167edeeb7
rohinimurugan02 <rohinimurugan02@gmail.com>
2026-07-25 09:55:37 +0530

    Create Day :10 (hw) 500 Keyword row.java

---
+++ Day :10 (hw) 500 Keyword row.java
@@ -0,0 +1,35 @@
+public class keyboard row {
+    public String[] findWords(String[] words) {
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        java.util.List<String> list = new java.util.ArrayList<>();
+
+        for (String word : words) {
+            String w = word.toLowerCase();
+
+            String row;
+            if (row1.indexOf(w.charAt(0)) != -1)
+                row = row1;
+            else if (row2.indexOf(w.charAt(0)) != -1)
+                row = row2;
+            else
+                row = row3;
+
+            boolean valid = true;
+
+            for (char c : w.toCharArray()) {
+                if (row.indexOf(c) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+                list.add(word);
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
