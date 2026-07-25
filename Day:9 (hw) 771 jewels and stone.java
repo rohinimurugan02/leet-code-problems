e48b7967079d41227ee265f8a1ea77856b0d3ee3
rohinimurugan02 <rohinimurugan02@gmail.com>
2026-07-25 09:30:17 +0530

    Implement method to count jewels in stones

---
+++ Day:9 (hw) 771 jewels and stone.java
@@ -0,0 +1,16 @@
+public class jewlesandstone {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            jewelSet.add(c);
+        }
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewelSet.contains(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
